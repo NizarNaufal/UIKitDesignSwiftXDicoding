ccb92c6258f0bc8a40eab558eda389cd6014dcaf
NizarNaufal <32515100+NizarNaufal@users.noreply.github.com>
2021-02-23 19:47:57 +0700

    fetch image

--- UIKitDesignDicoding/ViewController.swift
+++ UIKitDesignDicoding/ViewController.swift
@@ -15,5 +15,20 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func editProfile(_ sender: Any) {
+        let alert = UIAlertController(title: "Apakah Anda berniat untuk mengubah profil?",
+                message: "Anda dapat memperbarui profil Anda sekarang juga.",
+                preferredStyle: .alert)
+         
+            alert.addAction(UIAlertAction(title: "Ya", style: .default, handler: { action in
+                print("Mengganti profile.")
+            }))
+         
+            alert.addAction(UIAlertAction(title: "Tidak", style: .cancel, handler: { action in
+                print("Membatalkan ubah profil.")
+            }))
+         
+            self.present(alert, animated: true)
+    }
 }
 
